7e3439ad90f9d4ae97c3f0848e88eb6b709d1f88
William Pettersson <contact@williampettersson.eu>
2025-08-13 17:02:26 +0200

    Add table, constraints and insert to install script

--- install.js
+++ install.js
@@ -21,7 +21,28 @@ connection.addListener("error", (error) => {
 const sqlCommands = [
 	// Create database
 	"DROP DATABASE IF EXISTS `"+dbName+"`",
-	"CREATE DATABASE `"+dbName+"` CHARACTER SET = 'utf8mb4' COLLATE = 'utf8mb4_uca1400_ai_ci'"
+	"CREATE DATABASE `"+dbName+"` CHARACTER SET = 'utf8mb4' COLLATE = 'utf8mb4_uca1400_ai_ci'",
+	"USE `"+dbName+"`",
+	// Create table
+	"CREATE TABLE `courses` (`code` VARCHAR(6) NOT NULL, `name` VARCHAR(50), `syllabus` VARCHAR(100), `progression` VARCHAR(1))",
+	// Create constraints
+	"ALTER TABLE `courses` ADD CONSTRAINT COURSE_PK PRIMARY KEY (`code`)",
+	// Insert data
+	"INSERT INTO `courses` VALUES ('DT057G', 'Webbutveckling I', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT057G/', 'A')",
+	"INSERT INTO `courses` VALUES ('DT084G', 'Introduktion till programmering i JavaScript', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT084G/', 'A')",
+	"INSERT INTO `courses` VALUES ('DT200G', 'Grafisk teknik för webb', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT200G/', 'A')",
+	"INSERT INTO `courses` VALUES ('DT068G', 'Webbanvändbarhet', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT068G/', 'B')",
+	"INSERT INTO `courses` VALUES ('DT003G', 'Databaser', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT003G/', 'A')",
+	"INSERT INTO `courses` VALUES ('DT211G', 'Frontend-baserad webbutveckling', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT211G/', 'B')",
+	"INSERT INTO `courses` VALUES ('DT207G', 'Backend-baserad webbutveckling', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT207G/', 'B')",
+	"INSERT INTO `courses` VALUES ('DT208G', 'Programmering i TypeScript', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT208G/', 'B')",
+	"INSERT INTO `courses` VALUES ('IK060G', 'Projektledning', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/IK060G/', 'A')",
+	"INSERT INTO `courses` VALUES ('DT071G', 'Programmering i C#.NET', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT071G/', 'A')",
+	"INSERT INTO `courses` VALUES ('DT193G', 'Fullstack-utveckling med ramverk', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT193G/', 'B')",
+	"INSERT INTO `courses` VALUES ('DT209G', 'Webbutveckling för WordPress', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT209G/', 'B')",
+	"INSERT INTO `courses` VALUES ('DT191G', 'Webbutveckling med .NET', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT191G/', 'B')",
+	"INSERT INTO `courses` VALUES ('DT210G', 'Fördjupad frontend-utveckling', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT210G/', 'B')",
+	"INSERT INTO `courses` VALUES ('DT140G', 'Självständigt arbete', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT140G/', 'B')"
 ];
 
 // Run SQL commands
